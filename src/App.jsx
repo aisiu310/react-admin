c6ec12d9fee6aa74c61e25e025da10eff7359ec3
icey <icey@example.com>
2019-04-01 19:26:53 +0800

    添加了Login静态页面

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import Admin from './pages/admin';
+import Login from './pages/login';
+
+import './assets/less/reset.less'
+
+export default class App extends Component {
+    render(){
+        return(
+            <Switch>
+                <Route path='/login' component={Login}/>
+                <Route path='/' component={Admin}/>
+                <Redirect to='/login'/>
+            </Switch>
+        )
+    }
+}
